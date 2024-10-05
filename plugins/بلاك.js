8905cf6ddcb5a8c2f851c9fe4c204c7c5fdaeca1
EMAM-ABOLELA <160362844+EMAM-ABOLELA@users.noreply.github.com>
2024-10-06 02:33:17 +0300

    Create بلاك.js

---
+++ plugins/بلاك.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, { text, usedPrefix, command, conn }) => {
+  if (!text) {
+    throw "> *مرحبًا، أنا خدمة `Black Box Ai`، خدمة قادرة على برمجة الأكواد ومساعدتك في حياتك اليوميه المثال :*\n\n- #برمجة كيفية إنشاء صفحة تسجيل دخول باستخدام `html`";
+  }
+
+  try {
+    const apiURL = `https://artst-three.vercel.app/blackbox?text=${text}`;
+    const response = await fetch(apiURL);
+    const result = await response.json();
+
+    if (result.result && text.trim().length > 0) {
+      let captionn = result.result;  // النص الذي سيتم إرساله مع الصورة أو الرد
+
+      // إرسال الصورة مع الزر
+      await conn.sendButton(m.chat, captionn, ' > PLUTO | 🐼❤️', null, [['🔄 جرب مرة أخرى 🔄', `.بلاك ${text}`]], m);
+
+    } else {
+      throw '> *خطأ ⚠️: لم يتم العثور على نتيجة.*';
+    }
+
+  } catch (error) {
+    console.error(error);
+    throw '> *خطأ ⚠️: حدث خطأ أثناء الاتصال بالخدمة.*';
+  }
+};
+
+handler.command = /^(بلاك|بوكسي)$/i;
+handler.help = ['ai'];
+handler.tags = ['tool'];
+export default handler;
