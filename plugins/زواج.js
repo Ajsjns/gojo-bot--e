3572e972982e2a3eac499b2472abaf9200c6404d
EMAM-ABOLELA <160362844+EMAM-ABOLELA@users.noreply.github.com>
2024-10-06 03:49:06 +0300

    Update زواج.js

--- plugins/زواج.js
+++ plugins/زواج.js
@@ -1,16 +1,22 @@
-let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-m.reply(`*${toM(a)}, الف مبروك للعريس🥳✨*
-*${toM(b)}, الف مبروك للعروسه🥳✨*\n\*ملاحظه ده منشن عشوائي للمرح فقط اذ لم يعجبك الأمر إذن ماتستخدمش الامر💗*`, null, {
-mentions: [a, b]
-})}
-handler.help = ['formarpareja']
-handler.tags = ['main', 'fun']
-handler.command = ['formarpareja','زواج']
-handler.group = true
-export default handler
+let toM = a => '@' + a.split('@')[0];
+
+async function handler(m, { groupMetadata, conn }) {
+    let ps = groupMetadata.participants.map(v => v.id);
+    let a = ps.getRandom();
+    let b;
+    do b = ps.getRandom();
+    while (b === a);
+
+    const captionn = `*${toM(a)}, الف مبروك للعريس🥳✨*\n*${toM(b)}, الف مبروك للعروسه🥳✨*\n\n*ملاحظه: ده منشن عشوائي للمرح فقط، إذا لم يعجبك الأمر إذن ماتستخدمش الأمر💗*`;
+    const link = 'http://example.com'; // أدخل الرابط الذي تريد استخدامه هنا
+
+    // إرسال الزر مع الرسالة
+    await conn.sendButton(m.chat, captionn, ' > Gojo | 🐼❤️', link, [['🔄 اتنين كمان 👩‍❤️‍👨', `.زواج`]], m);
+}
+
+handler.help = ['formarpareja'];
+handler.tags = ['main', 'fun'];
+handler.command = ['formarpareja', 'زواج'];
+handler.group = true;
+
+export default handler;
